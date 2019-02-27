084211efbbbe7b2044b8c53ee79ce068d1d07676
Carlos-S <iznomag@gmail.com>
2019-02-27 16:19:58 -0500

    tarea1-lista

---
+++ bot_teenager.rb
@@ -0,0 +1,17 @@
+def bot_teenager
+    puts "Ingrese pregunta?"
+    question = gets.chomp
+    
+
+    if question.include?("!") && question.include?("?")
+        puts "Calm down, I know what I'm doing!"
+    elsif question.include?("!")
+        puts "Whoa, chill out!"
+    elsif question.include?("?")
+        puts "Sure"    
+    else
+        puts "Fine. Be that way!"
+    end
+end
+
+bot_teenager
